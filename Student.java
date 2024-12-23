47f03228eae63771e2390d5ec251f12bbaed84d3
Daoud Kashif <daoudkashif2004@gmail.com>
2024-12-23 18:34:23 -0500

    Code formatting

--- Student.java
+++ Student.java
@@ -14,6 +14,8 @@ public class Student extends School {
     public void setName(String n){
         this.name = n;
     }
+
+
     @Override
     public String toString(){
         return super.toString() + " " + name;
