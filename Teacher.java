9fd9d95d26a2d394cd0ad45a5aac10b4545db3a8
Daoud Kashif <daoudkashif2004@gmail.com>
2024-12-23 18:35:18 -0500

    added create/remove/add student functionality and roster ArrayList

--- Teacher.java
+++ Teacher.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+
 public class Teacher extends School{
-    public String name;
-    public String subject;
+    private String name;
+    private String subject;
+    private ArrayList<Student> roster;
 
     public Teacher(String d, String s, String n, String sub){
         super(d, s);
         this.name = n;
         this.subject = sub;
+        this.roster = new ArrayList<Student>();
     }
 
     public String getSubject(){
@@ -22,5 +26,25 @@ public class Teacher extends School{
 
     public void setName(String n){
         this.name = n;
+    }
+
+    //Not Sure if needed or places right
+    public void createStudent(String n){
+        Student student = new Student(super.getDistrict(), super.getSchoolName(), n);
+        roster.add(student);
+        System.out.println("Added student " + student.getName() + " to " + Teacher.this.getName() + "'s roster");
+    }
+
+    public void removeStudent(Student s){
+        roster.remove(s);
+    }
+
+    public ArrayList<Student> getRoster(){
+        return roster;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ": " + name + " Subject - " + subject;
     }
 }
