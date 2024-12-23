d02a96d611ca72d8098cfd25dc1e5dddf755b00b
Daoud Kashif <daoudkashif2004@gmail.com>
2024-12-22 21:30:46 -0500

    Added name

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,16 @@
 public class Teacher extends School{
-    
+    public String name;
+
+    public Teacher(String d, String s, String n){
+        super(d, s);
+        this.name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String n){
+        this.name = n;
+    }
 }
