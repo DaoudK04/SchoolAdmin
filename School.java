7b075e310aa31be1c7474c1bdc9435fbc20cf54c
Daoud Kashif <daoudkashif2004@gmail.com>
2024-12-22 21:19:48 -0500

    Added teacher variables

--- School.java
+++ School.java
@@ -1,18 +1,20 @@
-public class middleSchool extends District {
+public class School extends District {
     
     private String schoolName;
 
-    public middleSchool(String d, String s){
+    public School(String d, String s){
         super(d);
         schoolName = s;
     }
+
 
     public String getSchoolName(){
         return schoolName;
     }
 
+    @Override
     public String toString(){
-        return super.getDistrict() + " " + schoolName + " Middle School";
+         return super.getDistrict() + " " + schoolName;
     }
-    
+
 }
--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,12 @@
-public class Teacher extends elementarySchool{
-    
+public class Teacher extends School{
+
+    public String name;
+    public String subject;
+
+    public Teacher(String d, String s, String n, String sub){
+        super(d, s);
+        name = n;
+        subject = sub;
+    }
+
 }
