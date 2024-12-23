d360702aa50b42610ba3d70bd36453f6747b6a8e
TommyC34 <thomaskc09257@gmail.com>
2024-12-22 21:02:21 -0500

    Added code to District and eSchool

--- District.java
+++ District.java
@@ -1,4 +1,18 @@
 public class District {
 
+    private String district;
+    
+    public District(String d){
+        this.district = d;
+    }
+
+    public String getDistrict(){
+        return this.district;
+    }
+
+    @Override
+    public String toString(){
+        return district;
+    }
     
 }
--- elementarySchool.java
+++ elementarySchool.java
@@ -1,3 +1,19 @@
 public class elementarySchool extends District {
     
+    private String schoolName;
+
+    public elementarySchool(String d, String s){
+        super(d);
+        schoolName = s;
+    }
+
+
+    String getSchoolName(){
+        return schoolName;
+    }
+
+    String toString(){
+        return super.getDistrict() + " " + schoolName;
+    }
+
 }
