2a65bea8e23989120624137ab5b0a6be39d68877
Daoud Kashif <daoudkashif2004@gmail.com>
2024-12-23 18:33:34 -0500

    Added create/Add/remove teacher methods and staff arrayList

--- School.java
+++ School.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 public class School extends District {
     
     private String schoolName;
+    private ArrayList<Teacher> staff;
 
     public School(String d, String s){
         super(d);
         this.schoolName = s;
+        staff = new ArrayList<Teacher>();
     }
 
 
@@ -14,6 +18,21 @@ public class School extends District {
     
     public String getSchoolName(){
         return this.schoolName;
+    }
+
+    //Not sure if createTeacher is necessary same in Student.java
+    public Teacher createTeacher(String n, String sub){
+        return new Teacher(super.getDistrict(), this.schoolName, n, sub);
+    }
+
+    public void addTeacher(Teacher t){
+        staff.add(t);
+        System.out.println("Removed teacher " + t.getName() + " from " + this.schoolName);
+    }
+
+    public void removeTeacher(Teacher t){
+        staff.remove(t);
+        System.out.println("Removed teacher " + t.getName() + " from " + this.schoolName);
     }
 
     @Override
