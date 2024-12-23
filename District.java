ee1c0b68c5619aa3a341e058a71cd75d7445fb81
Daoud Kashif <daoudkashif2004@gmail.com>
2024-12-23 18:32:30 -0500

    Added setter and toString()

--- District.java
+++ District.java
@@ -6,6 +6,10 @@ public class District {
         this.district = d;
     }
 
+    public String setDistrict(String d){
+        return this.district = d;
+    }
+
     public String getDistrict(){
         return this.district;
     }
